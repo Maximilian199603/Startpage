32a4dc640df584327d10600e53cb573b4414b3f3
Maximilian1996 <magnitudetwende@gmail.com>
2025-01-29 19:35:00 +0100

    Added arrowkey listeners

--- scripts/toggleNav.js
+++ scripts/toggleNav.js
@@ -1,6 +1,6 @@
 
 // Function to toggle the left sidebar visibility
-function toggleNav() {
+function toggleNavleft() {
     var sidepanel = document.getElementById("mySidepanel");
     if (sidepanel.style.width === "0px" || sidepanel.style.width === "") {
 		sidepanel.style.width = "25vw";
@@ -18,3 +18,33 @@ function toggleNavright() {
         sidepanel.style.width = "0";
     }
 }
+
+// Function to hide both sidebars on page load
+function hideSidebars() {
+    var sidepanelLeft = document.getElementById("mySidepanel");
+    var sidepanelRight = document.getElementById("SidepanelRight");
+
+    // Hide both sidebars
+    sidepanelLeft.style.width = "0";
+    sidepanelRight.style.width = "0";
+    }
+
+// Call hideSidebars() function when the page is fully loaded
+    document.addEventListener("DOMContentLoaded", function() {
+        hideSidebars();
+        limitListElements();
+        limitListElementsRight();
+    });
+	
+document.addEventListener("keydown", (event) => {
+    if (event.repeat) return; // Prevents continuous firing when holding the key
+
+    switch (event.code) {
+        case "ArrowLeft":
+            toggleNavleft();
+            break;
+        case "ArrowRight":
+            toggleNavright();
+            break;
+    }
+});
